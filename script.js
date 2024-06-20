ef92fe8c36cd15ebd30bc562c73b30cba1007b5f
Spektra29 <bostonkarboston@gmail.com>
2024-06-19 22:24:22 -0400

    changes made

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('map-btn').addEventListener('click', function() {
+    window.open('map.html', '_blank');
+});
+
+document.getElementById('events-btn').addEventListener('click', function() {
+    window.open('events.html', '_blank');
+});
+
+document.getElementById('wrk-btn').addEventListener('click', function() {
+    window.open('workouts.html', '_blank');
+});
+
+document.getElementById('chat-btn').addEventListener('click', function() {
+    window.open('chat.html', '_blank');
+});
+
