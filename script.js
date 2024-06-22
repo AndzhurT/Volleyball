a4d976e11cf02e31a9e2e4158952c7c1a5a06f9e
MoonGGate <goryukvadim5@gmail.com>
2024-06-22 15:47:42 -0400

    Implement registration popup

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const popup = document.querySelector('.login-popup');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginButton = document.querySelector('.login-btn');
+const popupCloseButton = document.querySelector('.close-btn');
+
+registerLink.addEventListener('click', ()=> {
+    popup.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    popup.classList.remove('active');
+});
+
+loginButton.addEventListener('click', ()=> {
+    popup.classList.add('active-popup');
+});
+
+popupCloseButton.addEventListener('click', ()=> {
+    popup.classList.remove('active-popup');
+});
+
 document.getElementById('map-btn').addEventListener('click', function() {
     window.open('../map/map.html', '_self');
 });
@@ -17,11 +39,3 @@ document.getElementById('chat-btn').addEventListener('click', function() {
 document.getElementById('nav-sign').addEventListener('click', function() {
     window.open('../signup/sign.html', '_self');
 });
-
-function openLoginForm() {
-    document.getElementById('login-popup').style.opacity = 1;
-}
-
-function closeLoginForm() {
-    document.getElementById('login-popup').style.opacity = 0;
-}
