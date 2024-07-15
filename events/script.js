e58c9ef09ecb5a310316c86a40ea15d8b26cda3d
Spektra29 <bostonkarboston@gmail.com>
2024-07-15 18:50:57 -0400

    Created navbars for every other button tabs fixed login pop-up issue, wrote a window overriding code for both login and map.

--- events/script.js
+++ events/script.js
@@ -1,3 +1,54 @@
+// login - sign up popups
+const popup = document.querySelector('.login-popup');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginButton = document.querySelector('.login-btn');
+const popupCloseButton = document.querySelector('.close-btn');
+
+// hamburger 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerCheckbox = document.querySelector('.hamburger-checkbox');
+const navMenuList = document.querySelector('.vb-nav-menu-group');
+
+const items = document.querySelectorAll('.item');
+
+// navbar
+const navbar = document.querySelector('.navbar');
+
+registerLink.addEventListener('click', async () => {
+    popup.classList.add('active');
+});
+
+loginLink.addEventListener('click', async () => {
+    popup.classList.remove('active');
+});
+
+loginButton.addEventListener('click', async () => {
+    popup.classList.add('active-popup');
+});
+
+popupCloseButton.addEventListener('click', async () => {
+    popup.classList.remove('active-popup');
+});
+
+document.getElementById('nav-sign').addEventListener('click', async function() {
+    window.open('../signup/sign.html', '_self');
+});
+
+const hamburgerMenuPopup = async (ev) => {
+    if (hamburgerCheckbox.checked) {   
+        navbar.classList.add('active');
+        navMenuList.classList.add('active');
+        items.forEach(item => item.classList.add('active'));
+    } else {
+        navbar.classList.remove('active');
+        navMenuList.classList.remove('active');
+        items.forEach(item => item.classList.remove('active'));
+    }
+}
+
+hamburgerCheckbox.addEventListener('click', hamburgerMenuPopup);
+
 //Locations data
 
 document.getElementById('main-btn').addEventListener('click', function() {
